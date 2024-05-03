d3bb9942c77ec5ea756bd01455a18716a78832b9
Ibrahim Aboelyazed <ebrahem6076@gmail.com>
2024-05-04 01:57:27 +0300

    Updated documentation

--- lib/modules/articles/cubits/articles_cubit.dart
+++ lib/modules/articles/cubits/articles_cubit.dart
@@ -8,7 +8,15 @@ import 'package:hamzallc_task/modules/articles/articles.dart';
 part 'articles_cubit.freezed.dart';
 part 'articles_state.dart';
 
+/// A Cubit for managing articles state.
+///
+/// This Cubit provides methods to interact with articles,
+/// such as fetching most viewed articles
+/// and handling background events related to articles.
 class ArticlesCubit extends Cubit<ArticlesState> {
+  /// Initializes the ArticlesCubit with the specified [articlesService].
+  ///
+  /// [articlesService]: The service instance used to fetch articles.
   ArticlesCubit({
     required this.articlesService,
   }) : super(const ArticlesState.initial()) {
@@ -23,6 +31,11 @@ class ArticlesCubit extends Cubit<ArticlesState> {
 
   final ArticlesService articlesService;
 
+  /// Fetches the most viewed articles.
+  ///
+  /// Emits [ArticlesState.loading] state before fetching
+  /// and either emits [ArticlesState.failure]
+  /// on failure or [ArticlesState.loaded] with the fetched articles on success.
   Future<void> getMostViewedArticles() async {
     emit(const ArticlesState.loading());
     final response = await articlesService.getMostViewedArticles();
@@ -32,6 +45,11 @@ class ArticlesCubit extends Cubit<ArticlesState> {
     );
   }
 
+  /// Refreshes the articles with the provided [articles].
+  ///
+  /// Emits [ArticlesState.loading] state before refreshing
+  /// and [ArticlesState.loaded] with
+  /// the refreshed articles.
   void refreshArticles(List<Article> articles) {
     emit(const ArticlesState.loading());
     emit(ArticlesState.loaded(articles));
